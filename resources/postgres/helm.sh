495eb5d110045c1789fbda1ee8f55da3b5282559
oleksandr.koval <oleksandr.koval@stripo.email>
2023-07-07 09:00:24 +0300

    fix

--- resources/postgres/helm.sh
+++ resources/postgres/helm.sh
@@ -1,1 +1,2 @@
-helm -n stripo install postgres oci://registry-1.docker.io/bitnamicharts/postgresql --set global.postgresql.auth.postgresPassword=secret --set global.postgresql.auth.username=example --set global.postgresql.auth.password=secret --set image.tag=14
+#helm -n stripo install postgres oci://registry-1.docker.io/bitnamicharts/postgresql --set global.postgresql.auth.postgresPassword=secret --set global.postgresql.auth.username=example --set global.postgresql.auth.password=secret --set image.tag=14
+helm -n stripo upgrade --install postgres oci://registry-1.docker.io/bitnamicharts/postgresql -f values.yaml
